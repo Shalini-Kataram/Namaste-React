0299fa678a01a5a3db91c13e595df3d2e734cf71
Shalini-Kataram <kataramshalini1@gmail.com>
2024-11-27 13:14:47 +0530

    Episode 05

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
+import React, { useState } from "react";
 import ResturantCard from "./ResturantCard";
 import mockData from "../utils/mockData";
 
 const Body = () => {
+    const [listOfResturants, setListOfResturants] = useState(mockData);
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        const filtered = listOfResturants.filter(
+                            (res) => res.avgRating > 4.3
+                        );
+                        console.log(filtered);
+                        setListOfResturants(filtered);
+                    }}>
+                    Top Rated Resturants
+                </button>
+            </div>
             <div className="res-container">
-                {mockData.map((rescard) => (
+                {listOfResturants.map((rescard) => (
                     <ResturantCard key={rescard.id} resData={rescard} />
                 ))}
             </div>
